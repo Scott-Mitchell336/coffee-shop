6387bfbf93394e5fb661a6bf477a1bd695a3d14b
lesommers <lesbsommers@gmail.com>
2025-05-22 18:20:41 -0700

    updating folder name to front-end

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import Items from './pages/Items';
+import Items from './pages/ItemsPage';
 
 
 function Home() {
