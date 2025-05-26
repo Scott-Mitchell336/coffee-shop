100aa539e8d6deaa78226636a7ae8c724f6b2df7
lesommers <lesbsommers@gmail.com>
2025-05-26 16:12:06 -0700

    made some updates to the ItemsPage, ItemDetail, fetchWrapper pages

--- front-end/src/pages/ItemsPage.jsx
+++ front-end/src/pages/ItemsPage.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { getItems } from "../api/fetchWrapper";
 
-//Will show all the items in the menu
+
 const ItemsPage = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchMenu = async () => {
+      try {
+        const data = await getItems();
+        setItems(data);
+      } catch (error) {
+        console.error("Failed to fetch menu:", error);
+      }
+    };
+    fetchMenu();
+  }, []);
+
   return (
     <div>
       <h1>Menu</h1>
-      <p>See our full menu below.</p>
+      {items.length > 0 ? (
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>
+              <h2>{item.name}</h2>
+              <p>{item.description}</p>
+              <p>${item.price.toFixed(2)}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No items available.</p>
+      )}
     </div>
   );
 };
